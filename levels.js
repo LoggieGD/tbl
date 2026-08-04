6a5ea947d13138631e7681cfbbe31d042aa91086
D4rkDev <d4rkytreal@gmail.com>
2026-08-04 00:37:12 -0400

    Add files via upload

---
+++ levels.js
@@ -0,0 +1,10 @@
+export const demons = [
+    {
+        rank: 1,
+        name: "Grief",
+        creator: "Idk",
+        verifier: "To Be Doggie",
+        difficulty: "Extreme Demon",
+        thumbnail: "images/grief.png"
+    }
+];
