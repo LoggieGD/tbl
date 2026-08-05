48c96a7130ae5c6c2092aad04692cc440272ec1d
Loggie <d4rkytreal@gmail.com>
2026-08-05 03:22:00 -0400

    Reorder demons array to change rank order

--- levels.js
+++ levels.js
@@ -1,17 +1,5 @@
 export const demons = [
     {
-        rank: 2,
-        name: "Bad Grief",
-        creator: "Loggie",
-        verifier: "Loggie",
-        id: "Currently Not Available",
-        difficulty: "Extreme Demon",
-        thumbnail: "images/grief.png",
-        link: "https://www.youtube.com/watch?v=4LsdjyJHkEI",
-        victors: "None, You Could Be The First.",
-        isLegacy: false
-    },
-    {
         rank: 1,
         name: "Bad Amethyst",
         creator: "Idk",
@@ -20,6 +8,18 @@ export const demons = [
         difficulty: "Extreme Demon",
         thumbnail: "images/amethyst.png",
         link: "https://youtu.be/xT2kGu3gn2M",
+        victors: "None, You Could Be The First.",
+        isLegacy: false
+    },
+    {
+        rank: 2,
+        name: "Bad Grief",
+        creator: "Loggie",
+        verifier: "Loggie",
+        id: "Currently Not Available",
+        difficulty: "Extreme Demon",
+        thumbnail: "images/grief.png",
+        link: "https://www.youtube.com/watch?v=4LsdjyJHkEI",
         victors: "None, You Could Be The First.",
         isLegacy: false
     },
